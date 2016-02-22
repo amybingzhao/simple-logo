ebcbad704fc89e079fb5ad7d6757b8721fc131c5
azhao96 <azhao96@gmail.com>
2016-02-22 13:53:05 -0500

    added properties files for Parser to know num arguments for functions and function object name for each function command

--- src/Controller/Parser.java
+++ src/Controller/Parser.java
@@ -1,6 +1,9 @@
 package Controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 /**
  * This class is used to parse command strings into Command objects.
@@ -8,7 +11,13 @@ import java.util.List;
  *
  */
 public class Parser {
+	private static final String FUNCTION_TO_NUM_ARGS_PROPERTIES = "NumArgsForFunctions.properties";
+	private ResourceBundle FunctionToNumArgsProperties = ResourceBundle.getBundle(FUNCTION_TO_NUM_ARGS_PROPERTIES);
+	private static final String AVAILABLE_FUNCTIONS = "AvailableFunctions.properties";
+	private ResourceBundle myAvailableFunctions = ResourceBundle.getBundle(AVAILABLE_FUNCTIONS);
 	private List<Command> myCommands;
+	private Map<String, Integer> myNumArgsForUserDefinedCommandsMap;
+	private Map<String, Integer> myUserDefinedCommandsMap;
 	
 	/**
 	 * Parses a given string of commands into a List of commands.
