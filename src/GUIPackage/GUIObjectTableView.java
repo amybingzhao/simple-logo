0d7e3cf31202474d7b69de9b632ed1110e615a23
ANNIE <anniekatelyntang@gmail.com>
2016-02-24 19:54:41 -0500

    Create GUIObject hierarchy, worked on Factory class and Resources file

---
+++ src/GUIPackage/GUIObjectTableView.java
@@ -0,0 +1,24 @@
+package GUIPackage;
+
+import Controller.Controller;
+import javafx.scene.Node;
+
+public class GUIObjectTableView implements GUIObject {
+
+	public GUIObjectTableView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Node createNode(Controller c, String nodeType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateNode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
