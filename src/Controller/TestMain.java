ccecfcbf1ea2d47e7188e56aea93b651739aff2c
azhao96 <azhao96@gmail.com>
2016-02-26 01:05:00 -0500

    fixed so that whitespaces aren't added into inputCommandList

--- src/Controller/TestMain.java
+++ src/Controller/TestMain.java
@@ -5,6 +5,6 @@ public class TestMain {
 	public static void main(String[] args) throws ClassNotFoundException {
 		Controller c = new Controller();
 		c.init(100, 100);
-		c.processCommand("less? sum 1 2 50");
+		c.processCommand("less?	\t \n sum 1 2 50");
 	}
 }
