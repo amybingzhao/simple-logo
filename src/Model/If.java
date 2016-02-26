0a52d56b30a709733ef972c8296e52f2fb297278
azhao96 <azhao96@gmail.com>
2016-02-25 22:13:56 -0500

    added If function

---
+++ src/Model/If.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.util.List;
+
+public class If extends Node {
+
+	@Override
+	public int interpret() {
+		List<Node> children = getChildren();
+		int ret = 0;
+		
+		if (children.get(0).interpret() == 1) {
+			ret = children.get(1).interpret();
+		}
+		
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		List<Node> children = getChildren();
+		return "if " + children.get(0).toString() + " " + children.get(1).toString();
+	}
+
+}
