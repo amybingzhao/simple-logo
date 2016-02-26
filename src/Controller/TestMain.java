598ad454397aafaeb7c7397f0c6df08a5975c1f0
blakejkaplan <blakejkaplan@gmail.com>
2016-02-26 15:27:24 -0500

    Added Example to TestMain

--- src/Controller/TestMain.java
+++ src/Controller/TestMain.java
@@ -5,7 +5,6 @@ public class TestMain {
     public static void main(String[] args) throws ClassNotFoundException {
         Controller c = new Controller();
         c.init(100, 100);
-        //c.processCommand("ifelse equal? sum 1 2 2 [ fd 50 ] [ repeat 10 [ fd 20 fd 5 ] ]");
-        c.processCommand("for [:test 0 10 1] [fd :test]");
+        c.processCommand("for [ :test 0 10 1 ] [ fd :test ]");
     }
 }
