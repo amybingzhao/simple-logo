ac76a8b73e50d8f1c34e98691e4ca6addffcfedc
blakejkaplan <blakejkaplan@gmail.com>
2016-02-27 21:15:55 -0500

    Implemented Product

--- src/Model/Difference.java
+++ src/Model/Difference.java
@@ -6,6 +6,9 @@ import java.util.List;
  * Created by blakekaplan on 2/27/16.
  */
 public class Difference extends Node {
+
+    public static final String DIFFERENCE = "Difference ";
+
     @Override
     public double interpret() {
         List<Node> children = getChildren();
@@ -15,6 +18,6 @@ public class Difference extends Node {
 
     @Override
     public String toString() {
-        return "Difference " + getChildren().get(0).toString() + " " + getChildren().get(1).toString();
+        return DIFFERENCE + getChildren().get(0).toString() + " " + getChildren().get(1).toString();
     }
 }
