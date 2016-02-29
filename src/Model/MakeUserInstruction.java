7df1d0f395809f1f024ed9bfc2c99ba747e88201
azhao96 <azhao96@gmail.com>
2016-02-28 22:44:08 -0500

    commented makeuserinstruction and makevariable

--- src/Model/MakeUserInstruction.java
+++ src/Model/MakeUserInstruction.java
@@ -7,23 +7,28 @@ import java.util.List;
  */
 public class MakeUserInstruction extends Node {
 
+    private static final String TO = "to ";
+    private static final int NAME = 0;
+    private static final int VARIABLES = 1;
+    private static final int COMMANDS = 2;
     private String name;
 
-    public static final String TO = "MakeUserInstruction";
-
+    /**
+     * Creates a new user-defined command with the given name and parameters list that executes the given commands list.
+     */
+    //TODO: must return 0 if command not successfully defined
     public double interpret() {
-
         List<Node> children = getChildren();
-        name = children.get(0).toString();
+        name = children.get(NAME).toString();
         Command myCommand = new Command(name);
-        CommandList paramList = (CommandList) children.get(1);
+        CommandList paramList = (CommandList) children.get(VARIABLES);
         List<Node> parameters = paramList.getChildren();
         for (Node myNode : parameters) {
             Variable myVar = (Variable) myNode;
             myCommand.addParam(myVar.toString());
         }
         CommandDictionary.getInstance().setNumArguments(name, parameters.size());
-        CommandList expressions = (CommandList) children.get(2);
+        CommandList expressions = (CommandList) children.get(COMMANDS);
         myCommand.setProcedure(expressions.getChildren());
         CommandDictionary.getInstance().createCommand(name, myCommand);
         CommandDictionary myDict = CommandDictionary.getInstance();
@@ -31,18 +36,27 @@ public class MakeUserInstruction extends Node {
     }
 
     /**
-	 * Returns the required user input for this command. 
-	 */
-    @Override
-    public String toString() {
-        return TO;
-    }
-
+     * Gets the name of the command.
+     * @return command name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the command.
+     * @param newName: new name for the command.
+     */
     public void setName(String newName) {
         name = newName;
+    }
+    
+    /**
+	 * Returns the required user input for this command. 
+	 */
+    @Override
+    public String toString() {
+    	List<Node> children = getChildren();
+        return TO + children.get(NAME).toString() + " " + children.get(VARIABLES).toString() + " " + children.get(COMMANDS).toString();
     }
 }
--- src/Model/MakeVariable.java
+++ src/Model/MakeVariable.java
@@ -9,21 +9,17 @@ import java.util.List;
 public class MakeVariable extends Node {
 
     private static final String MAKE = "make ";
+    private static final int VARIABLE_NAME = 0;
+    private static final int EXPRESSION = 1;
     private String name;
 
     @Override
-    public String toString() {
-        return MAKE;
-    }
-
-    @Override
     public double interpret() {
-
         List<Node> children = getChildren();
-        String key = children.get(0).toString();
-        double value = children.get(1).interpret();
+        String key = children.get(VARIABLE_NAME).toString();
+        double value = children.get(EXPRESSION).interpret();
         VariableDictionary.getInstance().makeVariable(key, value);
-        return 0;
+        return value;
     }
 
     public String getName(){
@@ -32,5 +28,11 @@ public class MakeVariable extends Node {
 
     public void setName(String newName){
         name = newName;
+    }
+    
+    @Override
+    public String toString() {
+        List<Node> children = getChildren();
+        return MAKE + children.get(VARIABLE_NAME).toString() + " " + children.get(EXPRESSION).toString();
     }
 }
