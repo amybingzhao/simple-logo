5585ed84995375a59878b54f020402c723e72041
azhao96 <azhao96@gmail.com>
2016-02-28 22:36:57 -0500

    commented variabledictionary

--- src/Model/VariableDictionary.java
+++ src/Model/VariableDictionary.java
@@ -9,7 +9,12 @@ public class VariableDictionary {
 
     private static VariableDictionary instance = null;
     private static double DEFAULT = 0;
+    private Map<String, Double> myVariables;
 
+    /**
+     * Gets the instance of VariableDictionary; creates one if one does not yet exist.
+     * @return the instance of VariableDictionary.
+     */
     public static synchronized VariableDictionary getInstance(){
         if (instance == null){
             instance = new VariableDictionary();
@@ -17,32 +22,48 @@ public class VariableDictionary {
         return instance;
     }
 
-    private Map<String, Double> variables;
-
+    /**
+     * Initializes the variable map
+     */
     public VariableDictionary() {
-        variables = new HashMap<String, Double>();
+        myVariables = new HashMap<String, Double>();
     }
 
+    /**
+     * Makes a variable key/value pair.
+     * @param key: string expression for the variable name.
+     * @param value: current value of the variable.
+     */
     public void makeVariable(String key, double value) {
-        variables.put(key, value);
+        myVariables.put(key, value);
     }
 
-    // if it doesnt contain it, it should create the new variable
+    /**
+     * Gets the value associated with the variable name. If the variable does not yet exist, creates it and initializes its value to 0.
+     * @param key: variable name.
+     * @return variable value.
+     */
     public double getNodeFor(String key) {
         if (!contains(key)) {
-        	variables.put(key, DEFAULT);
+        	myVariables.put(key, DEFAULT);
         } //else throw new NullPointerException();
-        return variables.get(key);
+        return myVariables.get(key);
     }
 
+    /**
+     * Checks if a variable of the given name already exists.
+     * @param key: variable name to check for.
+     * @return true if the variable name already exists; false otherwise.
+     */
     public boolean contains(String key) {
-
-        if (variables.containsKey(key)) return true;
-        return false;
-
+        return myVariables.containsKey(key);
     }
 
+    /**
+     * Gets the set of variable names that already exist.
+     * @return set of existing variable names.
+     */
     public Set<String> getKeySet(){
-        return variables.keySet();
+        return myVariables.keySet();
     }
 }
