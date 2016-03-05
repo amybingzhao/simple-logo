6cb0f73f7c7c62e5c3f2d4033971238080e8e008
azhao96 <azhao96@gmail.com>
2016-03-05 15:59:51 -0500

    added setpencolor command

--- src/model/SetPenColor.java
+++ src/model/SetPenColor.java
@@ -1,5 +1,18 @@
 package model;
 
-public class SetPenColor {
+public class SetPenColor extends DisplayNode {
+	private static final String SET_PEN_COLOR = "setpencolor ";
+	private static final int INDEX = 0;
+	
+	@Override
+	public double interpret(CommandDictionary commandDict, VariableDictionary varDict)
+			throws ClassNotFoundException, NullPointerException, IndexOutOfBoundsException {
+		//getCanvas().setPenColor(getChildren().get(INDEX).interpret(commandDict, varDict));
+		return getChildren().get(INDEX).interpret(commandDict, varDict);
+	}
 
+	@Override
+	public String toString() {
+		return SET_PEN_COLOR + getChildren().get(INDEX).toString();
+	}
 }
