c11390cdc3a273eee6ebf9024ea974ccee2ac58d
David Yang <dy46@duke.edu>
2016-03-06 03:29:01 -0500

    added some pen settings code

--- src/guipackage/GUIObjectPenSettings.java
+++ src/guipackage/GUIObjectPenSettings.java
@@ -2,7 +2,13 @@ package guipackage;
 
 import java.util.ResourceBundle;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 
 public class GUIObjectPenSettings implements IGUIObject{
@@ -19,6 +25,32 @@ public class GUIObjectPenSettings implements IGUIObject{
 	public Node createNode() {
 		VBox myBox = new VBox(VBOX_SPACING);
 		
+		ToggleGroup penUpDownGroup = new ToggleGroup();
+		
+		RadioButton penUp = new RadioButton("Pen Up");
+		penUp.setToggleGroup(penUpDownGroup);
+		penUp.setSelected(true);
+		
+		RadioButton penDown = new RadioButton("Pen Down");
+		penDown.setToggleGroup(penUpDownGroup);
+		
+		
+		ToggleGroup penTypeGroup = new ToggleGroup();
+		
+		RadioButton penSolid = new RadioButton("Solid Pen");
+		penSolid.setToggleGroup(penTypeGroup);
+		penSolid.setSelected(true);
+		
+		RadioButton penDashed = new RadioButton("Dashed Pen");
+		penDashed.setToggleGroup(penTypeGroup);
+		
+		RadioButton penDotted = new RadioButton("Dotted Pen");
+		penDotted.setToggleGroup(penTypeGroup);
+		
+		
+		TextField penThickness = new TextField("Enter Desired Pen Thickness");
+		Button setThickness = new Button("Set Thickness");
+//		setThickness.setOnAction(event -> )
 		
 		
 		
