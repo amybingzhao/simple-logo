07ff0f46f19dacd09ac32c1e007e2b0ad2f5c58e
azhao96 <azhao96@gmail.com>
2016-03-10 20:42:37 -0500

    more comments

--- src/model/Turtles.java
+++ src/model/Turtles.java
@@ -3,25 +3,30 @@ package model;
 public class Turtles extends TurtleNode {
 
 	private static final String TURTLES = "turtles ";
+	
+	/**
+	 * Gets the number of existing turtles.
+	 */
 	@Override
     public double interpret(CommandDictionary commandDict, VariableDictionary varDict)
             throws ClassNotFoundException {
 		return getTurtles().size();
 	}
+	
+	/**
+	 * Not used in this class.
+	 */
+	@Override
+    protected double applyToIndividualTurtle(Turtle turtle, CommandDictionary commandDict, VariableDictionary varDict)
+            throws ClassNotFoundException {
+		return 0;
+	}
 
 	/**
-	 * Returns the class name and its children.
+	 * Returns the class name.
 	 */
 	@Override
 	public String toString() {
 		return TURTLES;
 	}
-
-	@Override
-    protected double applyToIndividualTurtle(Turtle turtle, CommandDictionary commandDict, VariableDictionary varDict)
-            throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
